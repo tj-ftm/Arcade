efc572b6fdd47354dcf9a07c76e3561c23e45b9e
tj-ftm <infinitedilutionttv@gmail.com>
2025-08-23 22:27:15 +0530

    Refactor: Remove WalletConnect and ensure MetaMask/Rabby options are available

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const ARC_TOKEN_ABI = [
+  "function balanceOf(address owner) view returns (uint256)",
+  "function mint(address to, uint256 amount) public",
+];
+export const ARC_TOKEN_CONTRACT_ADDRESS = "0x5a4676092495411072950E55954161e777095416";
+export const SONIC_CHAIN_ID = 146;
+export const SONIC_RPC_URL = "https://rpc.soniclabs.com";
