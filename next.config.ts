31ef4b533b7ec45a5755e2ffc90cf7b61a326978
tj-ftm <infinitedilutionttv@gmail.com>
2025-08-25 00:45:36 +0530

    fix: Add process polyfill to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = { process: require.resolve('process/browser') };
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
